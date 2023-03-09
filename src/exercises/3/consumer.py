ea5142b4a5feef06390f2e94755ac0b291dfc221
ale-rt <alessandro.pisa@gmail.com>
2023-03-09 14:25:07 +0100

    Auto delete the queues

--- src/exercises/3/consumer.py
+++ src/exercises/3/consumer.py
@@ -14,7 +14,7 @@ with pika.BlockingConnection(pika.ConnectionParameters("localhost")) as connecti
     # Read messages from exchange example3 which is of type topic
     # for routing keys that matched the routing_key variable
     channel.exchange_declare(exchange="example3", exchange_type="topic")
-    result = channel.queue_declare(queue="")
+    result = channel.queue_declare(queue="", auto_delete=True, durable=False)
     queue_name = result.method.queue
     channel.queue_bind(exchange="example3", queue=queue_name, routing_key=routing_key)
     channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
